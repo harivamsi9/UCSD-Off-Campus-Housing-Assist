fae4acc2de38656ccfbf05cc699c85e1cff83fcb
jiahuiz1 <jiahuiz@ksu.edu>
2023-02-16 23:28:38 -0800

    update 1

--- frontend/src/components/searchBar.test.js
+++ frontend/src/components/searchBar.test.js
@@ -3,28 +3,24 @@ import SearchBar from './searchBar';
 
 test('should render apply button', () => {
     render(<SearchBar />);
-    // const bedrommFilter = screen.getByPlaceholderText("Bedroom");
     const applyButton = screen.getByText("Apply");
     expect(applyButton).toBeInTheDocument();
 });
 
 test('should render sort high to low button', () => {
     render(<SearchBar />);
-    // const bedrommFilter = screen.getByPlaceholderText("Bedroom");
     const applyButton = screen.getByText("Sort By Price ↓");
     expect(applyButton).toBeInTheDocument();
 });
 
 test('should render sort low to high button', () => {
     render(<SearchBar />);
-    // const bedrommFilter = screen.getByPlaceholderText("Bedroom");
     const applyButton = screen.getByText("Sort By Price ↑");
     expect(applyButton).toBeInTheDocument();
 });
 
 test('should render bedroom filter', () => {
     render(<SearchBar />);
-    // const bedrommFilter = screen.getByPlaceholderText("Bedroom");
     const applyButton = screen.getByTestId("searchBar");
     expect(applyButton).toBeInTheDocument();
 });
