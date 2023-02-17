5a4d72063ea38c7d1bbed5b61df3fa6839afb056
jiahuiz1 <jiahuiz@ksu.edu>
2023-02-16 19:05:05 -0800

    searchBar

---
+++ src/components/searchBar.js
@@ -0,0 +1,89 @@
+import Select from 'react-select'
+import { bedOptions, bathOptions, locationOptions, priceOptions, squareFeet, commuteTime } from '../data/constant';
+import { useState } from 'react'
+import { testValues } from '../data/constant';
+import './searchBar.css';
+
+
+function SearchBar() {
+    const [json, setJson] = useState({});
+
+    const handleChangeBed = (selected) => {
+        setJson({ ...json, bedroom: selected.value })
+    };
+
+    const handleChangeBath = (selected) => {
+        setJson({ ...json, bathroom: selected.value })
+    };
+
+    const handleChangeLocation = (selected) => {
+        setJson({ ...json, location: selected.value })
+    };
+
+    const handleChangePrice = (selected) => {
+        setJson({ ...json, priceRange: selected.value })
+    };
+
+    const handleChangeArea = (selected) => {
+        setJson({ ...json, squareFeet: selected.value })
+    };
+
+    const handleChangeCommute = (selected) => {
+        setJson({ ...json, commuteTime: selected.value })
+    };
+
+    const handleApply = () => {
+        console.log(json)
+    }
+
+
+    return (
+        <div className="searchBar">
+            <Select
+                className="filter"
+                onChange={handleChangeBed}
+                closeMenuOnSelect={true}
+                options={bedOptions}
+                placeholder="Bedroorm">
+            </Select>
+            <Select
+                className="filter"
+                onChange={handleChangeBath}
+                closeMenuOnSelect={true}
+                options={bathOptions}
+                placeholder="Bathroom">
+            </Select>
+            <Select
+                className="filter"
+                onChange={handleChangePrice}
+                closeMenuOnSelect={true}
+                options={priceOptions}
+                placeholder="Price">
+            </Select>
+            <Select
+                className="filter"
+                onChange={handleChangeLocation}
+                closeMenuOnSelect={true}
+                options={locationOptions}
+                placeholder="Location">
+            </Select>
+            <Select
+                className="filter"
+                onChange={handleChangeArea}
+                closeMenuOnSelect={true}
+                options={squareFeet}
+                placeholder="Square Feet">
+            </Select>
+            <Select
+                className="filter"
+                onChange={handleChangeCommute}
+                closeMenuOnSelect={true}
+                options={commuteTime}
+                placeholder="Commute time">
+            </Select>
+            <button onClick={handleApply} id="applyButton">Apply</button>
+        </div>
+    );
+}
+
+export default SearchBar
---
+++ src/data/constant.js
@@ -0,0 +1,50 @@
+export const bedOptions = [
+    { value: '1 bedroom', label: '1 bedroom' },
+    { value: '2 bedrooms', label: '2 bedrooms' },
+    { value: '3 bedrooms', label: '3 bedrooms' },
+    { value: '4+ bedrooms', label: '4+ bedrooms' }
+];
+
+export const locationOptions = [
+    { value: 'La Jolla', label: 'La Jolla' },
+    { value: 'Old Town', label: 'Old Town' },
+    { value: 'Pacific Beach', label: 'Pacific Beach' },
+    { value: 'Mission Beach', label: 'Mission Beach' },
+    { value: 'Coronado Island', label: 'Coronado Island' },
+    { value: 'Tijuana', label: 'Tijuana' },
+    { value: 'Hillcrest', label: 'Hillcrest' }
+];
+
+export const bathOptions = [
+    { value: '1 bathroom', label: '1 bathroom' },
+    { value: '2 bathrooms', label: '2 bathrooms' },
+    { value: '3 bathrooms', label: '3 bathrooms' },
+    { value: '4+ bathrooms', label: '4+ bathrooms' }
+];
+export const priceOptions = [
+    { value: '1000-', label: '1000-' },
+    { value: '1000-2000', label: '1000-2000' },
+    { value: '2000-3000', label: '2000-3000' },
+    { value: '3000-4000', label: '3000-4000' },
+    { value: '4000+', label: '4000+' }
+];
+export const squareFeet = [
+    { value: '300-600', label: '300-600' },
+    { value: '600-1000', label: '600-1000' },
+    { value: '1000-1500', label: '1000-1500' },
+    { value: '1500+', label: '1500+' }
+];
+
+export const commuteTime = [
+    { value: '15min-', label: '15min-' },
+    { value: '15min-30min', label: '15min-30min' },
+    { value: '30min+', label: '30min' }
+]
+
+export const testValues = [
+    { bedroom: '1 bedroom', location: 'La Jolla', bathroom: '1 bathroom', price: 1230 },
+    { bedroom: '2 bedrooms', location: 'La Jolla', bathroom: '1 bathrooms', price: 1100 },
+    { bedroom: '2 bedrooms', location: 'Old Town', bathroom: '2 bathrooms', price: 2700 },
+    { bedroom: '2 bedrooms', location: 'Old Town', bathroom: '2 bathrooms', price: 2550 },
+    { bedroom: '3 bedrooms', location: 'Pacific Beach', bathroom: '2 bathrooms', price: 3500 }
+]
