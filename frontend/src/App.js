3de40f322370e0b20956ffdac39ae8637b08d870
jiahuiz1 <jiahuiz@ksu.edu>
2023-02-23 18:09:55 -0800

    fix the merge issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import SearchBar from './components/searchBar';
-import { useState } from 'react'
 import SearchPage from './pages/searchPage';
 import DetailPage from './pages/detailPage';
 import { Route, Router, Routes, Link } from 'react-router-dom'
@@ -12,12 +11,6 @@ function App() {
   // initial search page should be replaced with login page
   return (
     <div className="App">
-<<<<<<< HEAD
-        <SearchBar/>
-
-        {/*<p>{data.members}</p>*/}
-
-=======
       {/* <Router>
         <Routes>
           <Route path="/detail" element={<DetailPage />} />
@@ -26,7 +19,6 @@ function App() {
 
       {/* <DetailPage displayData={displayData} /> */}
       <SearchPage displayData={displayData} />
->>>>>>> 1acb24583364e3c8f2401d9a65b97f4d105735db
     </div>
   );
 }
