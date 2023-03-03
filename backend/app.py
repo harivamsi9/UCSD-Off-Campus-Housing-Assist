43818aad9745d70eab6989f2cb95578f0ca95436
harivamsi9 <harivamsiyadavalli@gmail.com>
2023-03-03 07:39:10 -0800

    commented out localhost and port

--- backend/app.py
+++ backend/app.py
@@ -80,8 +80,8 @@ if __name__ == '__main__':
     conn, cur = connect(conn, cur)
 
     app.run(
-        host='localhost',
-        port=5000,
+        # host='localhost',
+        # port=5000,
         debug=True,
     )
 
