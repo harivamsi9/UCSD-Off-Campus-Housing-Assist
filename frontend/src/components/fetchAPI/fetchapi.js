95067f254c6253342b8be9c7aca59132e50a1d39
harivamsi9 <harivamsiyadavalli@gmail.com>
2023-03-08 01:30:26 -0800

    added proper error message handling to the fetchAPI and made the fetch_filter_results as a async function

--- frontend/src/components/fetchAPI/fetchapi.js
+++ frontend/src/components/fetchAPI/fetchapi.js
@@ -1,29 +1,25 @@
 import { useState } from 'react'
 
 
-function fetch_filter_results(json) {
-    return fetch('/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        data: json
-      })
-    })
-    //   .then(res => res.json())
-    .then(response => {
+async function fetch_filter_results(json) {
+    try {
+        const response = await fetch('/search', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: json
+            })
+        });
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
-        return response.json();
-      })
-      .then(data => {
+        const data = await response.json();
         return data;
-      })
-      .catch(error => {
+    } catch (error) {
         console.error('Error fetching data:', error);
-      });
+    }
   }
 
 
